15b227c52c3a6071dee7b44b62acaf60044d9e5f
sheshum <36011215+sheshum@users.noreply.github.com>
2018-04-18 00:06:48 +0200

    style.css

--- backend/server/routes/api.js
+++ backend/server/routes/api.js
@@ -9,7 +9,7 @@ const userRouter = require('./route.user');
 router.post('/signup', ( req, res ) => authService.signupRoute( req, res ));
 router.post('/login', ( req, res ) => authService.loginRoute( req, res ));
 
-//router.use(authService.checkIfAuthenticated);
+router.use(authService.checkIfAuthenticated);
 
 //router.use('/users', userRouter);
 
