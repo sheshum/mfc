f3ca47fd85878d278a2b9a3d1b33968947e6b7c0
sheshum <36011215+sheshum@users.noreply.github.com>
2018-04-22 22:37:32 +0200

    sidebar controllers, user service

---
+++ frontend/app/components/sidebar/sidebar.controllers/contacts.controller/contacts.controller.js
@@ -0,0 +1,13 @@
+(function(){
+
+    angular.module('contacts')
+
+        .controller('ContactsController', ContactsController);
+
+        ContactsController.$inject[];
+        
+        function ContactsController() {
+
+        }
+
+})();
